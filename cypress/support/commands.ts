1b52ef2153501060328b046d19557c8157eb794c
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-09-23 17:45:03 +0100

    Add a command to unregister all service workers

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,6 +2,8 @@ declare namespace Cypress {
     interface Chainable {
         getById(id: string): Chainable<JQuery<HTMLElement>>
         getByClass(className: string): Chainable<JQuery<HTMLElement>>
+        
+        unregisterAllServiceWorkers(): void
     }
 }
 
@@ -11,4 +13,23 @@ Cypress.Commands.add('getById', (id: string) => {
 
 Cypress.Commands.add('getByClass', (className: string) => {
     return cy.get(`.${className}`)
+})
+
+Cypress.Commands.add('unregisterAllServiceWorkers', () => {
+    if (window.navigator && navigator.serviceWorker) {
+        navigator.serviceWorker.getRegistrations()
+        .then((registrations) => {
+            registrations.forEach((registration) => {
+                
+                Cypress.log({
+                    name: 'unregisterAllServiceWorkers',
+                    // shorter name for the Command Log
+                    displayName: 'Service Worker',
+                    message: `Unregistering: ${registration?.active?.scriptURL}`
+                })
+            
+                registration.unregister()
+            })
+        })
+    }
 })
