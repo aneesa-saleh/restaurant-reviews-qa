bd31e048f79f3fc368a50128b9259a9fd2b995a8
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-09-26 15:53:09 +0100

    Add a test to verify that the details page shows complete information

---
+++ cypress/e2e/details-page.spec.ts
@@ -0,0 +1,69 @@
+import { HomePageAPI } from "../support/api/HomePageAPI"
+import { DaysOfTheWeek } from "../support/common/constants"
+import { Restaurant } from "../support/models/restaurant"
+import { DetailsPage } from "../support/pages/DetailsPage"
+import { HomePage } from "../support/pages/HomePage"
+
+describe('Details page', () => {
+
+    let homePage: HomePage
+
+    beforeEach(() => {
+        homePage = new HomePage()
+    })
+
+    describe('details of a restaurant', () => {
+        it.only('shows complete details', () => {
+            const detailsPage = new DetailsPage()
+
+            homePage.clickViewDetailsLink(5).then(({ restaurantName: restaurantNameFromLink }) => {
+                detailsPage.API.getRestaurantDetails().then((restaurant: Restaurant) => {
+                        expect(restaurant).not.to.be.null
+                        expect(restaurant.name).to.equal(restaurantNameFromLink)
+
+                        cy.getById('restaurant-name')
+                            .should('have.length', 1)
+                            .and('have.text', restaurant.name)
+
+                        cy.getByClass('restaurant-img')
+                            .should('have.length', 1)
+                            .then((img) => {
+                                expect(img).to.have.attr('alt')
+                                expect(img.attr('alt')).to.equal(restaurant.alt)
+                            })
+
+                        cy.getById('restaurant-cuisine')
+                            .should('have.length', 1)
+                            .and('contain.text', restaurant.cuisine_type)
+
+                        cy.getById('map')
+                            .should('be.visible')
+                        
+                        cy.get('img.leaflet-marker-icon')
+                            .should('have.length', 1)
+                            .then((img) => {
+                                expect(img).to.have.attr('alt')
+                                expect(img.attr('alt')).to.equal(restaurant.name)
+                            })
+
+                        cy.getById('restaurant-address')
+                            .should('have.length', 1)
+                            .and('contain.text', restaurant.address)
+
+                        cy.getById('restaurant-hours')
+                            .should('be.visible')
+                            .then((openingHoursTable) => {
+                                expect(openingHoursTable.find('tr')).to.have.length(7)
+
+                                const daysOfTheWeek = Object.values(DaysOfTheWeek)
+
+                                daysOfTheWeek.forEach((day) => {
+                                    expect(openingHoursTable.find(`tr:contains("${day}")`)).to.have.length(1)
+                                })
+                            })
+                    })
+                })
+                
+        })
+    })
+})
