391ff67d18ed8f19653c547e58f9deca5d6a3736
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-09-26 08:38:08 +0100

    Add an intercept with stub for map tile api requests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -5,5 +5,7 @@ beforeEach(() => {
     // prevent unexpected behaviour with service workers
     // see: https://github.com/cypress-io/cypress/issues/702
     cy.unregisterAllServiceWorkers()
-    
+
+    // stub map tiles
+    cy.intercept(/^https:\/\/api.tiles.mapbox.com/g, { fixture: 'blank.jpg70,null', statusCode: 200 })
 })
