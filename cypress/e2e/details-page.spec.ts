dbca6a81988b3b2c9cee71bf025c674a4c6bb3ab
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-09-28 19:02:56 +0100

    Update restaurant details test to check for opening hour details

--- cypress/e2e/details-page.spec.ts
+++ cypress/e2e/details-page.spec.ts
@@ -1,3 +1,4 @@
+import { Utils } from "../support/common/Utils"
 import { DaysOfTheWeek } from "../support/common/constants"
 import { Restaurant } from "../support/models/restaurant"
 import { HomePage } from "../support/pages/HomePage"
@@ -10,54 +11,50 @@ describe('Details page', () => {
         homePage = new HomePage()
     })
 
-    describe('details of a restaurant', () => {
-        it('shows complete details', () => {
-            homePage.clickViewDetailsLink(5).then(({ restaurantName: restaurantNameFromLink, detailsPage }) => {
-                detailsPage.API.getRestaurantDetails().then((restaurant: Restaurant) => {
-                        expect(restaurant).not.to.be.null
-                        expect(restaurant.name).to.equal(restaurantNameFromLink)
+    it('shows complete details of a restaurant', () => {
+        homePage.clickViewDetailsLink(5).then(({ restaurantName: restaurantNameFromLink, detailsPage }) => {
+            detailsPage.API.getRestaurantDetails().then((restaurant: Restaurant) => {
+                    expect(restaurant).not.to.be.null
+                    expect(restaurant.name).to.equal(restaurantNameFromLink)
 
-                        detailsPage.getRestaurantName()
-                            .should('have.length', 1)
-                            .and('have.text', restaurant.name)
+                    detailsPage.getRestaurantName()
+                        .should('have.length', 1)
+                        .and('have.text', restaurant.name)
 
-                        detailsPage.getRestaurantImage()
-                            .should('have.length', 1)
-                            .and('have.attr', 'alt', restaurant.alt)
+                    detailsPage.getRestaurantImage()
+                        .should('have.length', 1)
+                        .and('have.attr', 'alt', restaurant.alt)
 
-                        detailsPage.getRestaurantCuisine()
-                            .should('have.length', 1)
-                            .and('contain.text', restaurant.cuisine_type)
+                    detailsPage.getRestaurantCuisine()
+                        .should('have.length', 1)
+                        .and('contain.text', restaurant.cuisine_type)
 
-                        detailsPage.getMap()
-                            .should('be.visible')
-                        
-                        detailsPage.getMapPin()
-                            .should('have.length', 1)
-                            .and('have.attr', 'alt', restaurant.name)
+                    detailsPage.getMap()
+                        .should('be.visible')
+                    
+                    detailsPage.getMapPin()
+                        .should('have.length', 1)
+                        .and('have.attr', 'alt', restaurant.name)
 
-                        detailsPage.getRestaurantAddress()
-                            .should('have.length', 1)
-                            .and('contain.text', restaurant.address)
+                    detailsPage.getRestaurantAddress()
+                        .should('have.length', 1)
+                        .and('contain.text', restaurant.address)
 
-                        detailsPage.getOpeningHours()
-                            .then((openingHours) => {
-                                expect(openingHours).to.be.visible
+                    detailsPage.getOpeningHours()
+                        .then((openingHours) => {
+                            expect(openingHours).to.be.visible
+                            expect(
+                                detailsPage.getOpeningHoursElements(openingHours)
+                            ).to.have.length(7)
+
+                            Utils.daysOfTheWeek().forEach((day) => {
                                 expect(
-                                    detailsPage.getOpeningHoursElements(openingHours)
-                                ).to.have.length(7)
-
-                                const daysOfTheWeek = Object.values(DaysOfTheWeek)
-
-                                daysOfTheWeek.forEach((day) => {
-                                    expect(
-                                        detailsPage.getOpeningHoursByDay(openingHours, day)
-                                    ).to.have.length(1)
-                                })
+                                    detailsPage.getOpeningHoursByDay(openingHours, day)
+                                ).to.have.length(1)
+                                .and.to.contain.text(restaurant.operating_hours[day])
                             })
-                    })
+                        })
                 })
-                
-        })
+            })
     })
 })
--- cypress/support/pages/DetailsPage.ts
+++ cypress/support/pages/DetailsPage.ts
@@ -9,6 +9,8 @@ export class DetailsPage {
         this.API = new DetailsPageAPI()
     }
 
+    /* Locators */
+
     getLocationPathname(): string {
         return '/restaurant.html'
     }
@@ -48,4 +50,124 @@ export class DetailsPage {
     getOpeningHoursByDay(openingHours: JQuery<HTMLElement>, day: DaysOfTheWeek): JQuery<HTMLElement> {
         return openingHours.find(`tr:contains("${day}")`)
     }
+
+    getAddReviewButton(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('add-review-button')
+    }
+
+    getModalTitle(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('add-review-overlay-heading')
+    }
+
+    getNameField(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('name')
+    }
+
+    getNameFieldError(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('name-error')
+    }
+
+    getRatingField(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('rating')
+    }
+
+    getRatingValue(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getByClass('rating-value')
+    }
+
+    getRatingFieldError(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('rating-error')
+    }
+
+    getCommentsField(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('comments')
+    }
+
+    getCommentsFieldError(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('comments-error')
+    }
+
+    getFormErrorMessage(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('add-review-form-error')
+    }
+
+    getSubmitReviewButton(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.getById('add-review-submit')
+    }
+
+    getCancelButton(): Cypress.Chainable<JQuery<HTMLButtonElement>> {
+        return cy.getByClass('overlay-content').contains('button', 'Cancel')
+    }
+
+    getReview(index: number): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get('article.review')
+            .should('have.length.greaterThan', index)
+            .eq(index)
+    }
+
+    getMostRecentReview(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return this.getReview(0)
+    }
+
+    getPendingReviews() {
+        return cy.get('article.review.sending')
+    }
+
+    getReviewElements(review: JQuery<HTMLElement>): ReviewElements {
+        return {
+            reviewerName: review.find('p.review-name'),
+            date: review.find('p.review-date'),
+            rating: review.find('p.review-rating'),
+            comment: review.find('.review-content p').last()
+
+        }
+    }
+
+    getSuccessToast(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get('toast.success.show')
+    }
+
+    getInfoToast(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get('toast.show')
+    }
+
+    getErrorToast(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get('toast.error.show')
+    }
+
+    /* UI actions */
+
+    clickAddReviewButton() {
+        this.getAddReviewButton().click()
+    }
+
+    typeName(name: string) {
+        this.getNameField().type(name)
+    }
+
+    chooseRating(rating: number) {
+        this.getRatingField()
+            .invoke('val', rating)
+            .trigger('change')
+    }
+
+    typeComment(comment: string) {
+        this.getCommentsField().type(comment)
+    }
+
+    clickSubmitReviewButton() {
+        this.getSubmitReviewButton().click()
+    }
+
+    clickCancelButton() {
+        this.getCancelButton().click()
+    }
+
+}
+
+type ReviewElements = {
+    reviewerName: JQuery<HTMLElement>;
+    date: JQuery<HTMLElement>;
+    rating: JQuery<HTMLElement>;
+    comment: JQuery<HTMLElement>;
 }
