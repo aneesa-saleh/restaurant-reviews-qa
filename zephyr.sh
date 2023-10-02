59b0491a75b8742454f6ad1a9c14c75ad1c1a290
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-10-02 16:49:20 +0100

    Update zephyr.sh with correct project key and folder id

--- zephyr.sh
+++ zephyr.sh
@@ -2,7 +2,7 @@
 
 authorization_header="Authorization: Bearer $ZEPHYR_SCALE_TOKEN"
 
-project_key="PRAC"
+project_key="RR"
 auto_create_test_cases="true"
 upload_results_url="https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=$project_key&autoCreateTestCases=$auto_create_test_cases"
 
@@ -11,13 +11,13 @@ file_path="file=@\"$junit_xml_file_path\""
 
 current_date=$(date +"%d-%m-%Y %T")
 test_cycle_name="Regression Tests - $current_date"
-test_cycle_folder_id="9784070"
+test_cycle_folder_id="9938623"
 test_cycle_json_body="testCycle=
     \"{\\\"name\\\":\\\"$test_cycle_name\\\",
     \\\"folderId\\\": $test_cycle_folder_id
     }\";
     type=application/json"
-    
+
 curl --location --request POST "$upload_results_url" \
 --header "$authorization_header" \
 --form "$file_path" \
