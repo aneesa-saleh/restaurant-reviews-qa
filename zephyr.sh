0c9f93c2993d063e44e36618c288842d9adacaed
Aneesa Saleh <aneesa.saleh@gmail.com>
2023-10-02 15:26:53 +0100

    Add a bash script to publish test report to zephyr scale

---
+++ zephyr.sh
@@ -0,0 +1,24 @@
+# Publish test results to zephyr scale
+
+authorization_header="Authorization: Bearer $ZEPHYR_SCALE_TOKEN"
+
+project_key="PRAC"
+auto_create_test_cases="true"
+upload_results_url="https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=$project_key&autoCreateTestCases=$auto_create_test_cases"
+
+junit_xml_file_path="cypress/results/junit/results.xml\""
+file_path="file=@\"$junit_xml_file_path\""
+
+current_date=$(date +"%d-%m-%Y %T")
+test_cycle_name="Regression Tests - $current_date"
+test_cycle_folder_id="9784070"
+test_cycle_json_body="testCycle=
+    \"{\\\"name\\\":\\\"$test_cycle_name\\\",
+    \\\"folderId\\\": $test_cycle_folder_id
+    }\";
+    type=application/json"
+    
+curl --location --request POST "$upload_results_url" \
+--header "$authorization_header" \
+--form "$file_path" \
+--form "$test_cycle_json_body"
